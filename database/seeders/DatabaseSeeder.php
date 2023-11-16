957a23ce3b5b519e9247a7ef2c40cfe286e9eced
Nehal-Mahdy <nehalhassan914@yahoo.com>
2023-11-16 19:39:19 +0200

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,16 +22,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(GovernorateSeeder::class); //has an insertion api request
-        // $this->call(CitySeeder::class); //has an insertion api request
-        // $this->call(DaySeeder::class);
-        // $this->call(UserSeeder::class);
-        // $this->call(ClientsTableSeeder::class);
-        // $this->call(PharmacySeeder::class);
-        // $this->call(DeliverySeeder::class);
+        $this->call(GovernorateSeeder::class); //has an insertion api request
+        $this->call(CitySeeder::class); //has an insertion api request
+        $this->call(DaySeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(ClientsTableSeeder::class);
+        $this->call(PharmacySeeder::class);
+        $this->call(DeliverySeeder::class);
         // $this->call(PharmacyDayOffSeeder::class); // error
-        // $this->call(UserPhonesSeeder::class); 
-        // $this->call(CategorySeeder::class); 
+        $this->call(UserPhonesSeeder::class); 
+        $this->call(CategorySeeder::class); 
         $this->call(MedicationSeeder::class); 
         $this->call(PharmacyMedicationSeeder::class); 
         $this->call(OrderSeeder::class); 
